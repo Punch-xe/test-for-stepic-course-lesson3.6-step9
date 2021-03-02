6a0c872faf63dea99e55a6b4479bd2f10f1b7cce
Punch-xe <aftar.xe@gmail.com>
2021-03-02 12:24:45 +0300

    добавил альтернативную проверку в тест

--- test_items.py
+++ test_items.py
@@ -1,10 +1,14 @@
 # тест на наличие кнопки Добавить в корзину
+# закоментированы альтернативные способы проверки
+
 import time
 
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
 def test_submit_button_is_present(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(30)
-    add_to_cart = len(browser.find_elements_by_css_selector('.btn.btn-lg.btn-primary'))
-    assert add_to_cart > 0, 'no any button "Add to Cart"'
+    time.sleep(3)
+    assert browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"), 'no any button "Add to Cart"'
+    # add_to_cart = len(browser.find_elements_by_css_selector('.btn.btn-lg.btn-primary'))
+    # assert add_to_cart > 0, 'no any button "Add to Cart"'
 
