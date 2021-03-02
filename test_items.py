4075ba4b629d5adcdf6c92128eefcd3320b4c7d6
Punch-xe <aftar.xe@gmail.com>
2021-03-02 17:57:25 +0300

    changed test name

--- test_items.py
+++ test_items.py
@@ -5,10 +5,10 @@ import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_submit_button_is_present(browser):
+
+def test_button_add_to_cart_is_present(browser):
     browser.get(link)
     time.sleep(30)
     assert browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"), 'no any button "Add to Cart"'
     # add_to_cart = len(browser.find_elements_by_css_selector('.btn.btn-lg.btn-primary'))
     # assert add_to_cart > 0, 'no any button "Add to Cart"'
-
